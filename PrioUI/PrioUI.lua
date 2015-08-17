b8b707a2cc90e02a330c0168e5061097f10cb8c5
Michael A <michael.albertz@web.de>
2015-08-17 18:18:17 +0200

    test

--- PrioUI/PrioUI.lua
+++ PrioUI/PrioUI.lua
@@ -14,5 +14,5 @@ function PrioUI:Initialize()
 	if  E.private.install_complete == "prioincomplete" then PrioUI:Install(); end
 	EP:RegisterPlugin(addon,self.GetOptions)
 end
-
+-- test
 E:RegisterModule(PrioUI:GetName());
